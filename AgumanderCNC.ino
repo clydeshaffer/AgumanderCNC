fd152ca9f9c9af1379d3ba19f662cb72262e8d57
Clyde Shaffer <cs@adcade.com>
2014-09-30 15:20:02 -0400

    read instructions from SD card

--- AgumanderCNC.ino
+++ AgumanderCNC.ino
@@ -1,9 +1,10 @@
-#include <Wire.h>
+  #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
 #include <SPI.h>
 #include <Pixy.h>
 #include <elapsedMillis.h>
+#include <SD.h>
 
 //REQUIRED LIBRARIES (LICENCED SEPARATELY)
 ////ElapsedMillis
@@ -48,6 +49,8 @@ int yMax = 0;
 int lastReportedX;
 int lastReportedY;
 
+File dataFile;
+
 //returns x position as 0-1000 value based on min and max
 int convertX(int rawX) {
   return ((rawX - xMin) * (10000 / (xMax - xMin))) / 10;
@@ -58,7 +61,7 @@ int convertY(int rawY) {
   return ((rawY - yMin) * (10000 / (yMax - yMin))) / 10;
 }
 
-int maxSpeed = 196;
+int maxSpeed = 128;
 //speedValue ranges from -100 to 100, as percentage of maxSpeed
 void setSpeedPercent(Adafruit_DCMotor *motor, int speedValue) {
   if(speedValue > 100) speedValue = 100;
@@ -78,25 +81,18 @@ void setSpeedPercent(Adafruit_DCMotor *motor, int speedValue) {
 //if doMove is set FALSE then xPos and yPos are unchanged
 //toolPower only gets like three bits of resolution, at current
 boolean readNextCmd (int *xPos, int *yPos, int *toolPower) {
-  char timeoutCount = 0;
-  char cmdBuf[3];
-  while(Serial.available() < 3) {
-    delay(100);
-    if((timeoutCount++) > 50) return false;
-  }
-  Serial.readBytes(cmdBuf, 3);
-  
-  if(cmdBuf[2] & 16 != 0) {
-    *xPos = cmdBuf[0] | ((((int) cmdBuf[1]) & 3) << 8);
-    *yPos = (cmdBuf[1] >> 2) | ((((int) cmdBuf[2]) & 15) << 8);
-    //*doMove = true;
+  if(dataFile.available()) {
+    int x, y, tool;
+    x = dataFile.parseInt();
+    y = dataFile.parseInt();
+    tool = dataFile.parseInt();
+    if(x >= 0 && x <= 1000) *xPos = x;
+    if(y >= 0 && y <= 1000) *yPos = y;
+    if(tool >= 0 && tool <= 255) *toolPower = tool;
+    return true;
   } else {
-    //*doMove = false;
+    return false;
   }
-  
-  //resolution is 3 bits, or 0-7 range. scaling to 0-100 range in case later protocol has more bandwidth
-  //could precalculate (100/7) but multiplying by 100 then div by 7 each time should preserve precision better
-  *toolPower = ((int) (cmdBuf[2] >> 5)) * 255 / 7;
 }
 
 void setLaserPower(int dutyCycle) {
@@ -107,17 +103,35 @@ void setLaserPower(int dutyCycle) {
 
 //Called on boot
 void setup() {
+  pinMode(laserPin, OUTPUT);
+  setLaserPower(0);
+  
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Agumander's Cheapo CNC - Online!");
-  Serial.setTimeout(6000);
+  Serial.setTimeout(10000);
 
+  Serial.println("Initializing Pixy...");
   pixy.init();
 
+  Serial.println("Initializing motors...");
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   xAxis->setSpeed(0);
   yAxis->setSpeed(0);
   zAxis->setSpeed(0);
+  
+  Serial.print("\nInitializing SD card...");
+  pinMode(10, OUTPUT);
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+  if(SD.exists("data.agu")) Serial.println("data.agu exists");
+  dataFile = SD.open("data.agu");
+  if(dataFile) Serial.println("Found instruction data!");
+  else Serial.println("ERROR: Data not found.");
+  Serial.println("Setup Complete!");
 }
 
 //Called over and over again automatically
@@ -125,7 +139,7 @@ void loop() {
   uint16_t blocks;
   char buf[32]; 
 
-  //get this frame's beacon position  
+  //get this frame's beacon position 
   blocks = pixy.getBlocks();
   if (blocks)
   {
@@ -148,6 +162,8 @@ void loop() {
   if(cncMode == NONE) {
     cncMode = CALIBRATE;
     timeElapsed = 0;
+    sprintf(buf, "At (%d, %d)", lastReportedX, lastReportedY);
+    Serial.println(buf);
     Serial.print("MODE IS CALIBRATE\n");
   } 
   else if(cncMode == CALIBRATE) {
@@ -171,12 +187,13 @@ void loop() {
   else if(cncMode == GOTO_TARGET) {
     int deltaX = target_x - convertX(lastReportedX);
     int deltaY = target_y - convertY(lastReportedY);
-
-    //two comparisons might be faster than two mults, an add, and a comparison
-    //square imprecision area should contribute slightly to temporal accuracy
+    
     if(abs(deltaX) < 20 && abs(deltaY) < 20) {
+      //TODO: Normalize speeds so axes arrive at target simultaneously
       setSpeedPercent(xAxis, 0);
       setSpeedPercent(yAxis, 0);
+      Serial.print("Reading commands... bytes available: ");
+      Serial.println(Serial.available());
       if(readNextCmd(&target_x, &target_y, &toolDutyCycle)) {
         Serial.print("Moving to: ");
         sprintf(buf, "(%d, %d) ", target_x, target_y);
@@ -186,7 +203,7 @@ void loop() {
         setLaserPower(toolDutyCycle);
       } else {
         Serial.print("CONNECTION TIMED OUT!\n");
-        cncMode = AT_TARGET;
+        cncMode = AT_TARGET;  
         setLaserPower(0);
       }
     } 
@@ -200,5 +217,3 @@ void loop() {
     setSpeedPercent(yAxis, 0);
   }
 }
-
-
